4d4a997948c60276e2d2ddf97801908374814ab2
myfreeweb <floatboth@me.com>
2013-08-06 19:13:04 +0400

    update singular

--- jquery.wideplate.js
+++ jquery.wideplate.js
@@ -3,12 +3,12 @@ jQuery.fn.extend({
     return this.each(function() {
       var $this = jQuery(this);
       $this.css('width', 'auto');
-      if (!$this.hasClass('narrow-collapse') || typeof matchMedia == 'undefined' || matchMedia('screen and (max-width: 32em)').matches) {
-        var aid = jQuery('<div>').css('height', '0px').appendTo($this);
-        var winhei = jQuery(window).height();
-        $this.css('width', aid.position().left);
-        aid.detach();
-      }
+      var aid = jQuery('<div>').css('height', '0px').appendTo($this);
+      var winhei = jQuery(window).height();
+      var width = aid.position().left + aid.width();
+      $this.css('width', width);
+      jQuery('html').css('min-width', width);
+      aid.detach();
     });
   }
 });
